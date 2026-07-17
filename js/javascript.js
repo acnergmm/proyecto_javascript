1481e7b08b93fcb20a9f7046b5560880f8e7438d
acnergmm <apinazo@gmartinmartin.com>
2026-07-16 23:43:37 -0300

    Add initial variable declarations and logging functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,46 @@
-let variableName = "Hello, World!";
-console.log(variableName);
+  
+  let myVariable2 = "Hello, World 3!";
+    const myVariable3 = "Hello, World 4!";
+
+
+let nombre = "Juan";
+
+let apellido = "Perez";
+
+
+let edad = 30;
+
+
+let activo = true 
+
+
+    
+
+
+function muestraLog() {  
+
+
+    console.log(myVariable2);
+    console.log(myVariable3);
+
+
+
+}
+
+
+function muestraNombre(){
+    console.log(nombre);
+    console.log(apellido);
+}
+
+
+
+
+
+
+
+
+
+
+
+
