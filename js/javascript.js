1d789aa048fd6387981f17729fcd2c4b9a810bea
acnergmm <apinazo@gmartinmartin.com>
2026-07-16 23:50:01 -0300

    test

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,27 @@
   
+<<<<<<< Updated upstream
 let myVariable2 = "Hello, World 3!";
 const myVariable3 = "Hello, World 4!";
 
 
 let nombre = "Juan";
+=======
+  let myVariable2 = "Hello, World 3!";
+    const myVariable3 = "Hello, World 4!";
+
+
+let nombre = "Juan";
+
+>>>>>>> Stashed changes
 let apellido = "Perez";
 
 
 let edad = 30;
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 let activo = true 
 
 
@@ -15,8 +29,18 @@ let activo = true
 
 
 function muestraLog() {  
+<<<<<<< Updated upstream
     console.log(myVariable2);
     console.log(myVariable3);
+=======
+
+
+    console.log(myVariable2);
+    console.log(myVariable3);
+
+
+
+>>>>>>> Stashed changes
 }
 
 
@@ -36,3 +60,7 @@ function muestraNombre(){
 
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
