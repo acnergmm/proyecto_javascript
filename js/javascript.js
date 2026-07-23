905fa28eb49f54ec0aef0d33ae97a1c3295f1a5b
acnergmm <apinazo@gmartinmartin.com>
2026-07-23 19:12:34 -0300

    Remove merge conflict markers and clean up variable declarations in javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,21 @@
   
-<<<<<<< Updated upstream
 let myVariable2 = "Hello, World 3!";
 const myVariable3 = "Hello, World 4!";
 
 
 let nombre = "Juan";
-=======
   let myVariable2 = "Hello, World 3!";
     const myVariable3 = "Hello, World 4!";
 
 
 let nombre = "Juan";
 
->>>>>>> Stashed changes
 let apellido = "Perez";
 
 
 let edad = 30;
-<<<<<<< Updated upstream
-=======
 
 
->>>>>>> Stashed changes
 let activo = true 
 
 
@@ -29,18 +23,15 @@ let activo = true
 
 
 function muestraLog() {  
-<<<<<<< Updated upstream
-    console.log(myVariable2);
-    console.log(myVariable3);
-=======
-
-
     console.log(myVariable2);
     console.log(myVariable3);
 
 
+    console.log(myVariable2);
+    console.log(myVariable3);
 
->>>>>>> Stashed changes
+
+
 }
 
 
